1651053959078ede544f481c2dbd71b25826f893
Katrina Mitchell <kat.hoffert@gmail.com>
2019-02-17 17:39:20 -0500

    Start to addon
    Currently logs time in console

---
+++ netflix-srt-subs.js
@@ -0,0 +1,37 @@
+const DEBUG = true;
+
+function main() {
+	if(DEBUG) console.log('Netflix SRT subs active');
+
+	// Get the URL without any query parameters or anchors
+	let url = window.location.href;
+	url = url.split('?')[0];
+	url = url.split('#')[0]; // In case there is no query parameters
+
+	// URL is, eg, https://www.netflix.com/watch/70136341 -- want the 70136341
+	let urlSplitOnSlash = url.split('/');
+	let videoId = urlSplitOnSlash[urlSplitOnSlash.length - 1];
+
+	if(DEBUG) console.log(`Netflix SRT subs URL: ${url} Video ID: ${videoId}`);
+
+	// We seem to be on a video page
+	if(videoId.match(/\d+/)) {
+		if(DEBUG) console.log(`Netflix SRT subs detected this page to be a video (id = ${videoId})`);
+		setInterval(() => {
+			console.log(getTimeInVideo(videoId));
+		}, 500)
+	}
+}
+
+function getTimeInVideo(videoId) {
+	// https://stackoverflow.com/a/42047162/1968462
+	let video = document.evaluate(`//*[@id="${videoId}"]/video`, document).iterateNext();
+	if(video) {
+		return video.currentTime;
+	}
+	else {
+		return null;
+	}
+}
+
+main();

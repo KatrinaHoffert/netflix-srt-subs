fa5b815a5504fdf5c406f96190ca5930c02b1351
Katrina Mitchell <kat.hoffert@gmail.com>
2019-02-17 19:00:25 -0500

    Replaced browser action
    File pickers do NOT work in them. :(

--- browser-action.js
+++
@@ -1,7 +0,0 @@
-let button = document.querySelector('#submit');
-button.onclick = () => {
-	browser.tabs.query({active: true}, (tabs) => {
-		let currentTabId = tabs[0].id;
-		browser.tabs.sendMessage(currentTabId, {'srt_data': 'fake SRT data'});
-	});
-};
--- netflix-srt-subs.js
+++ netflix-srt-subs.js
@@ -37,18 +37,61 @@ function getTimeInVideo(videoId) {
 	}
 }
 
-/**
- * Receives the SRT text from the background script.
- */
-function receiveSrtText(message) {
-	if('srt_data' in message) {
-		console.log(`RECEIVED: ${message['srt_data']}`);
-	}
-	else {
-		console.log(`Received unexpected message: ${JSON.stringify(message)}`);
-	}
-	return Promise.resolve();
+// https://stackoverflow.com/a/35385518/1968462
+function htmlToElement(html) {
+	let template = document.createElement('template');
+	html = html.trim(); // Never return a text node of whitespace as the result
+	template.innerHTML = html;
+	return template.content.firstChild;
 }
-browser.runtime.onMessage.addListener(receiveSrtText);
+
+/**
+ * Displays a file picker to select an SRT file. This must be done since browser
+ * actions will close the moment a file picker is opened, which makes it impossible
+ * to have any interaction at all.
+ */
+function displaySrtFilePicker() {
+	// Remove if existing
+	let existingElement = document.getElementById('netflix-srt-subs-main-box');
+	if(existingElement) {
+		existingElement.outerHTML = '';
+	}
+
+	// TODO: Make this auto-hide
+	let html = htmlToElement(`<div style="position: fixed; top: 0; left: 0; background-color: white;
+			color: black; z-index: 1; padding: 0.25em;"
+			class="netflix-srt-subs-main-box">
+			Load subs:
+			<input type="file" id="netflix-srt-subs-file-picker" style="display: none;">
+			<input type="button" value="Browse..." onclick="document.getElementById('netflix-srt-subs-file-picker').click();">
+		</div>`);
+	document.body.appendChild(html);
+
+	let fileInput = document.getElementById('netflix-srt-subs-file-picker');
+	fileInput.addEventListener('change', () => {
+		var file = fileInput.files[0];
+		if(file) {
+			if(DEBUG) console.log(`Netflix SRT subs loading file ${file}`);
+			var reader = new FileReader();
+			reader.readAsText(file, "UTF-8");
+			reader.onload = (ev) => {
+				let fileContents = ev.target.result;
+
+				// TODO: parse and make subs
+				console.log(fileContents)
+			};
+			reader.onerror = () => {
+				// TODO: handle nicely
+				console.log(` Netflix SRT subs error occurred reading file ${file}`)
+			};
+		}
+		else {
+			if(DEBUG) console.log('Netflix SRT subs no file selected');
+		}
+	});
+}
+
+// TODO: detect when video changes and remove subs
 
 main();
+displaySrtFilePicker();
